e57b9135d627f51ea09e1d8961c84c4eea50899a
Jaival Saija <saijajaival@gmail.com>
2023-05-02 15:03:45 +0530

    Update _document.tsx
    Solved crossOrigin Type error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin='true'/>
+            crossOrigin='anonymous'/>
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Nunito:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"></link>

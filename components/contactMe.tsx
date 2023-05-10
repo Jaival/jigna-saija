b79027c3781b800563d91e921d5b331bbd88ae87
Jaival Saija <saijajaival@gmail.com>
2023-05-10 15:30:43 +0530

    Update contactMe.tsx
    Solved the issue with the QR properly not showing in mobile view and added facebook icon

--- components/contactMe.tsx
+++ components/contactMe.tsx
@@ -62,9 +62,8 @@ export default function ContactMeComponent() {
               Fill in the details and I&apos;ll get back to you as soon as I can.
             </p>
           </header>
-          <div className="flex flex-row justify-between my-10 icons-container md:inline-flex md:flex-col">
-            <div
-              className="flex flex-row items-center p-4 space-x-6 border rounded-md border-white-dark">
+          <div className="flex flex-col justify-between my-10 md:inline-flex">
+            <div className="flex flex-row items-center p-4 space-x-6 border rounded-md border-white-dark">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -81,56 +80,46 @@ export default function ContactMeComponent() {
               </p>
             </div>
 
-            <div className='flex flex-col pt-4 rounded-lg'>
+            <div className='flex flex-row items-center p-4 space-x-6 rounded-md '>
               <Image src={'/images/jignasaijaQR.png'} width={200} height={200} alt='qr-code' priority></Image>
             </div>
 
           </div>
+          
           <div className="flex flex-row space-x-8 social-icons">
 
             <Link
               href={userData.socialLinks.facebook}
-              className="flex items-center justify-center w-10 h-10 rounded-lg cursor-pointer hover:border hover:border-aero"
+              className="flex items-center justify-center w-10 h-10 rounded-lg cursor-pointer hover:border hover:border-white-dark"
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                className="w-6 h-6"
                 fill="currentColor"
-                className="w-5 h-5 bi bi-twitter"
-                viewBox="0 0 16 16"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
               >
                 <path
-                  d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                  fillRule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clipRule="evenodd"
+                />
               </svg>
             </Link>
+            
             <Link
               href={userData.socialLinks.instagram}
-              className="flex items-center justify-center w-10 h-10 rounded-lg cursor-pointer hover:border hover:border-aero"
+              className="flex items-center justify-center w-10 h-10 rounded-lg cursor-pointer hover:border hover:border-white-dark"
             >
               <svg
-                width="24"
-                height="24"
-                className=""
+                className="w-6 h-6"
+                fill="currentColor"
                 viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
               >
                 <path
                   fillRule="evenodd"
+                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
                   clipRule="evenodd"
-                  d="M12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7ZM9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
-                  fill="currentColor"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5 1C2.79086 1 1 2.79086 1 5V19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5ZM19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
-                  fill="currentColor"
                 />
               </svg>
             </Link>

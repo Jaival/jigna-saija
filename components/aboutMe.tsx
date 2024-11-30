0f5371955f08cc773e572e8c0357830925b4390c
Jaival <saijajaival@gmail.com>
2024-12-01 01:44:05 +0530

    feat: add motion library and implement animations in hero and about me sections

--- components/aboutMe.tsx
+++ components/aboutMe.tsx
@@ -1,52 +1,103 @@
+import { motion } from 'motion/react';
 import Image from 'next/image';
+import { useRef } from 'react';
 
 export default function AboutMeComponent() {
+  const experience = {
+    '20': {
+      description: 'Years of Experience',
+    },
+    '15': {
+      description: 'Projects Completed',
+    },
+  };
+  const scrollRef = useRef(null);
   return (
-    <div className='flex md:justify-center md:items-center'>
+    <div className="flex md:justify-center md:items-center">
       <div className="container mt-14">
         <div className="my-10 text-left md:text-center">
-          <h2 className='text-xl font-bold md:text-4xl'>About Me</h2>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            className="text-xl font-bold md:text-4xl"
+          >
+            About Me
+          </motion.h2>
         </div>
 
         <div className="flex flex-col items-center gap-2 md:flex-row">
-          <div className="flex flex-col">
+          <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            viewport={{ root: scrollRef }}
+            className="flex flex-col"
+          >
             <Image
               className="rounded-lg aspect-auto"
-              alt="architecture"
+              alt="jigna saija"
               width={3248}
               height={4062.1}
-              src={
-                '/images/jignasaija.jpg'
-              }
+              src={'/images/jignasaija.jpg'}
             />
             <div className="py-2 text-left md:text-center">
-              <q className="font-light md:text-base">We believe in complete design</q>
-              <p className="text-sm font-thin md:text-base md:h-6"> - Jigna Saija </p>
+              <q className="font-light md:text-base">
+                I believe in complete design
+              </q>
+              <p className="text-sm font-thin md:text-base md:h-6">
+                {' '}
+                - Jigna Saija{' '}
+              </p>
             </div>
-          </div>
+          </motion.div>
 
-          <div className='flex flex-col md:ml-10'>
-            <p className='text-base leading-relaxed tracking-wide md:text-2xl'>
-              With more than fifteen years of experience designing both residential and commercial architectures and interiors, I gracefully oversee projects from concept to completion. As a versatile designer, I have an innate ability to conceive elegant and inspiring concepts and transform them into functional spaces that are both beautiful and practical.
+          <motion.div
+            initial={{ x: 25, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ root: scrollRef }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            className="flex flex-col md:ml-10"
+          >
+            <p className="text-base leading-relaxed tracking-wide md:text-2xl">
+              With more than fifteen years of experience designing both
+              residential and commercial architectures and interiors, I
+              gracefully oversee projects from concept to completion. As a
+              versatile designer, I have an innate ability to conceive elegant
+              and inspiring concepts and transform them into functional spaces
+              that are both beautiful and practical.
             </p>
-          </div>
+          </motion.div>
         </div>
 
         {/* <!-- Experience Cards--> */}
         <div className="grid grid-flow-col grid-rows-3 gap-4 mt-10 md:grid-rows-1">
-
-          <div className="card">
-            <div className="p-4 card-body">
-              <h5 className="text-lg font-bold text-white md:text-2xl">20</h5>
-              <p className="text-base md:text-lg text-white-dark">Years of Experience</p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="p-4 card-body">
-              <h5 className="text-lg font-bold text-white md:text-2xl">10</h5>
-              <p className="text-base md:text-lg text-white-dark">Projects Completed</p>
-            </div>
-          </div>
+          {Object.entries(experience).map(([key, value]) => (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ root: scrollRef }}
+              whileHover={{ scale: 1.1 }}
+              className="card"
+              key={key}
+            >
+              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                {key}
+              </h5>
+              <p className="text-base font-normal md:text-lg text-white-dark">
+                {value.description}
+              </p>
+            </motion.div>
+          ))}
         </div>
       </div>
     </div>
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,22 +1,96 @@
+import { motion } from 'motion/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRef } from 'react';
 
 export default function Hero() {
-  return (
-    <div className='mt-10'>
-      <div className="flex flex-row gap-5">
-        <div className="flex flex-col gap-6 md:justify-center md:items-center md:m-8">
-          <h1 className="text-xl md:text-5xl">Building the future, one project at a time.</h1>
-          <p className="w-auto text-l md:text-2xl">
-            Turning your big ideas into impressive reality.
-          </p>
-          <Link href='/projects'
-            className="px-4 py-2 font-bold text-center text-white rounded bg-button-blue hover:bg-honolulu-blue w-60">
-            See projects
-          </Link>
-        </div>
+  const whychoseme = {
+    'Design Approach': {
+      description:
+        'We hear the customer, we set the customers and their requirements in the heart of our design.',
+    },
+    'Transparency': {
+      description:
+        'We provide complete transparency on all matters. Our BOGs (bill of quality) & bring clarity in each step/material we select and order',
+    },
+    'Trust': {
+      description:
+        'We work to build trust. Our work & transparency speaks it all.',
+    },
+    'Quality': {
+      description:
+        'The design approach helps us offer well-planned, beautifully detailed long-lasting & high-satisfactory products.',
+    },
+    'Fast & Hassle Free': {
+      description:
+        'We handle the project from start to end. Clients need not co-ordinate when with team & contractors. We will manage all when the total responsibility has been given.',
+    },
+  };
 
-        <div className="hidden md:flex md:flex-col">
+  const scrollRef = useRef(null);
+
+  return (
+    <div ref={scrollRef} className="mt-10">
+      <div className="flex flex-col md:flex-row gap-5">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{
+            ease: 'anticipate',
+            duration: 1,
+          }}
+          viewport={{ root: scrollRef }}
+          className="flex flex-col gap-6 md:justify-center md:items-center md:m-8"
+        >
+          <motion.h1
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            viewport={{ root: scrollRef }}
+            className="text-xl md:text-5xl"
+          >
+            Building the future, one project at a time.
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            viewport={{ root: scrollRef }}
+            className="w-auto text-l md:text-2xl"
+          >
+            Turning your big ideas into impressive reality.
+          </motion.p>
+          <motion.div
+            className="flex px-4 py-2"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+          >
+            <Link
+              href="/projects"
+              className="px-4 py-2 font-bold text-center text-white rounded bg-button-blue hover:bg-honolulu-blue w-60"
+            >
+              See projects
+            </Link>
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ root: scrollRef }}
+          transition={{
+            ease: 'anticipate',
+            duration: 1,
+          }}
+          className=" md:flex md:flex-col"
+        >
           <Image
             className="rounded-lg"
             alt="architecture"
@@ -27,67 +101,42 @@ export default function Hero() {
               '/images/architecture_projects/project_pic_1/project_pic_2.jpg'
             }
           />
-        </div>
+        </motion.div>
       </div>
-      <div className='flex md:justify-center md:items-center'>
-        <div className='container mt-14'>
+
+      <div className="flex mt-40 md:mt-24 md:justify-center md:items-center">
+        <div className="container mt-14">
           <div className="mb-10 text-left md:text-center">
-            <p className='text-xl font-bold md:text-4xl'>
+            <motion.p
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              className="text-xl font-bold md:text-4xl"
+            >
               Why Chose Me?
-            </p>
+            </motion.p>
           </div>
 
-          <div className='grid grid-flow-col grid-rows-5 gap-4 md:grid-rows-2 md:gap-14'>
-            <div className="card">
-              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
-                Design Approach
-              </h5>
-              <p className="text-base font-normal md:text-lg text-white-dark">
-                We hear the customer, we set the customers and their requirements
-                in the heart of our design.
-              </p>
-            </div>
-
-            <div className="card">
-              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
-                Transparency
-              </h5>
-              <p className="text-base font-normal md:text-lg text-white-dark">
-                We provide complete transparency on all matters. Our BOGs (bill of
-                quality) & bring clarity in each step/material we select and
-                order.
-              </p>
-            </div>
-
-            <div className="card">
-              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
-                Trust
-              </h5>
-              <p className="text-base font-normal md:text-lg text-white-dark">
-                We work to build trust. Our work & transparency speaks it all.
-              </p>
-            </div>
-
-            <div className="card">
-              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
-                Quality
-              </h5>
-              <p className="text-base font-normal md:text-lg text-white-dark">
-                The design approach helps us offer well-planned, beautifully
-                detailed long-lasting & high-satisfactory products.
-              </p>
-            </div>
-
-            <div className="card">
-              <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
-                Fast & Hassle Free
-              </h5>
-              <p className="text-base font-normal md:text-lg text-white-dark">
-                We handle the project from start to end. Clients need not
-                co-ordinate when with team & contractors. We will manage all when
-                the total responsibility has been given.
-              </p>
-            </div>
+          <div
+            // ref={scrollRef}
+            className="grid grid-flow-col grid-rows-5 gap-4 md:grid-rows-2 md:gap-14"
+          >
+            {Object.entries(whychoseme).map(([key, value]) => (
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className="card"
+                key={key}
+              >
+                <h5 className="mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  {key}
+                </h5>
+                <p className="text-base font-normal md:text-lg text-white-dark">
+                  {value.description}
+                </p>
+              </motion.div>
+            ))}
           </div>
         </div>
       </div>

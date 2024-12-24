2192e03a69aeb739ffb181990444c67e012037f2
Jaival <saijajaival@gmail.com>
2024-12-25 02:50:18 +0530

    refactor: revamped design process component with animations and cleaned up styles

--- components/designProcess.tsx
+++ components/designProcess.tsx
@@ -1,109 +1,112 @@
-import { useTheme } from 'next-themes';
-import { useInView } from 'react-intersection-observer';
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import { motion } from 'motion/react';
+import { useRef } from 'react';
+import { ArrowRightIcon } from './ui/arrow-right';
 
 export default function DesignProcessComponent() {
-  const { theme, setTheme } = useTheme();
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  });
+  const scrollRef = useRef(null);
+
   return (
     <div className="flex md:justify-center md:items-center">
       <div className="container mt-5">
         <div className="my-10 text-left md:text-center">
-          <h2 className="font-bold text:xl md:text-4xl">Design Process</h2>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              ease: 'anticipate',
+              duration: 1,
+            }}
+            className="font-bold text:xl md:text-4xl"
+          >
+            Design Process
+          </motion.h2>
         </div>
-        <div ref={ref} className="container">
-          <VerticalTimeline lineColor={theme === 'dark' ? 'white' : 'black'}>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              visible={inView}
-              contentStyle={{ background: '#0E79B2', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #0E79B2' }}
-              iconStyle={{ background: '#0E79B2', color: '#fff' }}
-            >
-              <h3 className="font-semibold vertical-timeline-element-title text-l md:text-2xl">
-                Layout Options
-              </h3>
-              {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-              <p className="text-base">
-                We create a couple of format alternatives with the aid of using
-                knowledge of clients requirements, needs, and choices.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              visible={inView}
-              // date="November 2012"
-              iconStyle={{ background: '#A40E4C', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #A40E4C' }}
-              contentStyle={{ background: '#A40E4C', color: '#fff' }}
-              // icon={<SchoolIcon />}
-            >
-              <h3 className="font-semibold vertical-timeline-element-title text-l md:text-2xl">
-                Presentation
-              </h3>
-              {/* <h4 className="vertical-timeline-element-subtitle">Certification</h4> */}
-              <p className="text-base">
-                Considering customers preferences, we create several design
-                concepts for you to choose.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              visible={inView}
-              contentStyle={{ background: '#0E79B2', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #0E79B2' }}
-              iconStyle={{ background: '#0E79B2', color: '#fff' }}
-            >
-              <h3 className="font-semibold vertical-timeline-element-title text-l md:text-2xl">
-                3D Views
-              </h3>
-              {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-              <p className="text-base">
-                After concept confirmation, we only detail and create 3D if
-                necessary.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              visible={inView}
-              // date="November 2012"
-              iconStyle={{ background: '#A40E4C', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #A40E4C' }}
-              contentStyle={{ background: '#A40E4C', color: '#fff' }}
-              // icon={<SchoolIcon />}
-            >
-              <h3 className="font-semibold vertical-timeline-element-title text-l md:text-2xl">
-                Bill of Quality
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">BOQ</h4>
-              <p className="text-base">
-                Based on the material selection, we create detailed cost
-                estimates.
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              visible={inView}
-              contentStyle={{ background: '#0E79B2', color: '#fff' }}
-              contentArrowStyle={{ borderRight: '7px solid  #0E79B2' }}
-              iconStyle={{ background: '#0E79B2', color: '#fff' }}
-            >
-              <h3 className="font-semibold vertical-timeline-element-title text-l md:text-2xl">
-                Material Selection
-              </h3>
-              {/* <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4> */}
-              <p className="text-base">
-                Depending on your budget, we offer your preferred material
-                selection and combination.
-              </p>
-            </VerticalTimelineElement>
-          </VerticalTimeline>
+        <div className="container">
+          <div className="flex gap-4">
+            <div className="flex-1 flex-row">
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className={'mb-4 min-h-64 rounded-xl border-2 card p-4'}
+              >
+                <h3 className="pt-1 pb-3 mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  Layout Options
+                </h3>
+                <p className="text-lg">
+                  We create a couple of format alternatives with the aid of
+                  using knowledge of clients requirements, needs, and choices.
+                </p>
+              </motion.div>
+            </div>
+            <div className="flex-1">
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className={'mb-4 min-h-32 rounded-xl border-2 card p-4'}
+              >
+                <h3 className="pt-1 pb-3 mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  Presentation
+                </h3>
+                <p className="text-lg">
+                  Considering customers preferences, we create several design
+                  concepts for you to choose.
+                </p>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className={'mb-4 min-h-32 rounded-xl border-2 card p-4'}
+              >
+                <h3 className="pt-1 pb-3 mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  3D Views
+                </h3>
+                <p className="text-lg">
+                  After concept confirmation, we only detail and create 3D if
+                  necessary.
+                </p>
+              </motion.div>
+            </div>
+            <div className="flex-1">
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className={'mb-4 min-h-32 rounded-xl border-2 card p-4'}
+              >
+                <h3 className="pt-1 pb-3 mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  Bill of Quality
+                </h3>
+                <p className="text-lg">
+                  Based on the material selection, we create detailed cost
+                  estimates.
+                </p>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ root: scrollRef }}
+                whileHover={{ scale: 1.1 }}
+                className={
+                  'flex flex-col mb-4 min-h-32 rounded-xl border-2 card p-4'
+                }
+              >
+                <h3 className="pt-1 pb-3 mb-2 font-bold tracking-tight text-white text-l md:text-3xl">
+                  Material Selection
+                </h3>
+                <p className="text-lg">
+                  Depending on your budget, we offer your preferred material
+                  selection and combination.
+                </p>
+              </motion.div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
